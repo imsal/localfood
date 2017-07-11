f0516e1e0f7140081ca3543141789ffc5fe9506f
Sal Tardibuono <s.tardibuono@live.com>
2017-07-11 14:00:39 -0700

    had to comment out old / un-needed migration files to prepare for postgresql/heroku database reset

--- db/migrate/20170706052852_rename_password_digestc.rb
+++ db/migrate/20170706052852_rename_password_digestc.rb
@@ -1,5 +1,7 @@
+=begin
 class RenamePasswordDigestc < ActiveRecord::Migration[5.1]
   def change
     rename_column :users, :password_digestc, :password_digest
   end
 end
+=end
--- db/migrate/20170706163938_change_state_to_int_for_users.rb
+++ db/migrate/20170706163938_change_state_to_int_for_users.rb
@@ -1,6 +1,8 @@
+=begin
 class ChangeStateToIntForUsers < ActiveRecord::Migration[5.1]
   def change
     remove_column :users, :state
     add_column :users, :state, :integer
   end
 end
+=end
