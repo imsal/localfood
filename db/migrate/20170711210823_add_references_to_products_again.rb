8ed198dd8d1e6454ed548bcff75991ff9633cf43
Sal Tardibuono <s.tardibuono@live.com>
2017-07-11 14:09:41 -0700

    recreated refference to user migration file to occur after user table creation

--- db/migrate/20170711210823_add_references_to_products_again.rb
+++ db/migrate/20170711210823_add_references_to_products_again.rb
@@ -1,4 +1,4 @@
-class AddReferencesToProducts < ActiveRecord::Migration[5.1]
+class AddReferencesToProductsAgain < ActiveRecord::Migration[5.1]
   def change
     add_reference :products, :user, foreign_key: true
   end
