5e2653046ae553226ad7403f6f16c86cd340f301
Sal Tardibuono <s.tardibuono@live.com>
2017-07-11 14:22:16 -0700

    had to alter user_id refference again due to the following error ERROR:  column "user_id" of relation "products" does not exist

--- db/migrate/20170711210823_add_references_to_products_again.rb
+++ db/migrate/20170711210823_add_references_to_products_again.rb
@@ -1,6 +1,5 @@
 class AddReferencesToProductsAgain < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :products, :user
     add_reference :products, :user, foreign_key: true
   end
 end
